c0e2f7af361a3941e62d85d66b781e6dea60061a
suxue1229 <suxue7330537@163.com>
2020-04-14 10:25:04 +0800

    将chart作成组件，实现父子组件传值

---
+++ src/views/overview/chart.vue
@@ -0,0 +1,139 @@
+<template>
+  <div class="chart">
+    <div class="toggle">
+      <el-button class="button col-md-2" type="primary" @click="getlevel(1)">日视图</el-button>
+      <el-button class="button col-md-2" type="primary" @click="getlevel(2)">周视图</el-button>
+      <el-button class="button col-md-2" type="primary" @click="getlevel(3)">月视图</el-button>
+    </div>
+    <canvas id="planet-chart"></canvas>
+  </div>
+</template>
+
+<script>
+import Chart from 'chart.js'
+export default {
+  name: 'drawchart',
+  data () {
+    return {
+      id: '',
+      name: '',
+      level: 1,
+      time: this.formatDateTime(new Date()),
+      myChart: ''
+    }
+  },
+  props: {
+    sensor: {
+      type: Object,
+      required: true
+    }
+  },
+  /* 子组件mounted->父组件mounted->watch */
+  watch: {
+    sensor (newval) {
+      this.id = newval.Id
+      this.name = newval.Name
+      this.initchart(this.id, this.level)
+    }
+  },
+  methods: {
+    initchart (id, level) {
+      if (this.myChart) {
+        this.myChart.destroy()
+      }
+      this.$axios.get(this.HOST + '/sensor/' + id + '?level=' + this.level + '&time=' + this.time)
+        .then(res => {
+          if (res.data.status === 0) {
+            this.chartdata = res.data.data
+            var label1 = []
+            var data1 = []
+            for (var i = 0; i < this.chartdata.his.length; i++) {
+              label1.push(this.chartdata.his[i].time)
+              data1.push(this.chartdata.his[i].value)
+            }
+            let data = {
+              labels: label1,
+              datasets: [
+                {
+                  label: this.name,
+                  data: data1,
+                  backgroundColor: [
+                    'rgba(71, 183,132,.5)' // Green
+                  ],
+                  borderColor: [
+                    '#47b784'
+                  ],
+                  borderWidth: 3
+                }
+              ]
+            }
+            this.drawChart('planet-chart', data)
+          }
+        })
+        .catch(error => { console.log(error) })
+    },
+    getlevel (m) {
+      this.level = m
+      this.initchart(this.id, this.level)
+    },
+    // 时间戳转日期格式
+    formatDateTime (date) {
+      var y = date.getFullYear()
+      var m = date.getMonth() + 1
+      m = m < 10 ? ('0' + m) : m
+      var d = date.getDate()
+      d = d < 10 ? ('0' + d) : d
+      var h = date.getHours()
+      h = h < 10 ? ('0' + h) : h
+      var minute = date.getMinutes()
+      minute = minute < 10 ? ('0' + minute) : minute
+      var second = date.getSeconds()
+      second = second < 10 ? ('0' + second) : second
+      return y +  m +  d +  h + minute
+    },
+    drawChart (chartId, chartData) {
+      var ctx = document.getElementById(chartId)
+      this.myChart = new Chart(ctx, {
+        type: 'line',
+        data: chartData,
+        options: {
+          responsive: true,
+          lineTension: 1,
+          scales: {
+            yAxes: [{
+              ticks: {
+                beginAtZero: true,
+                padding: 25
+              }
+            }]
+          }
+        }
+      })
+    }
+  }
+}
+</script>
+
+<style scoped>
+.chart{
+  width: 100%;
+  height: 100%;
+}
+.chartpart .toggle{
+height: 80px;
+display: flex;
+}
+.chartpart .toggle .button{
+  margin:auto;
+  text-align: center;
+  transition: all 0.5s;
+}
+.chartpart .toggle .button:focus {
+  background-color: rgb(55, 137, 219);
+  border-color: rgb(55, 137, 219);
+}
+#planet-chart{
+  margin: 30px auto;
+}
+
+</style>
--- src/views/overview/monitor.vue
+++ src/views/overview/monitor.vue
@@ -14,48 +14,49 @@
           <h4>仪表数据</h4>
           <table class="table table-striped">
             <tr v-for="(ite,index) in infos[1]" :key="index">
-              <td class="col_name"><a  href="#" @click="initchart(ite.Id,level,index)" class="astyle">{{ite.Name}}</a></td>
+              <td class="col_name"><a id="tag_a" href="javascript:void(0)" class="astyle" @click="sendItem(ite)">{{ite.Name}}</a></td>
               <td class="col_value">{{ite.Value}} {{ite.Unit}}</td>
             </tr>
           </table>
       </div>
       <div class="chartpart">
-        <div class="toggle">
-          <el-button class="button col-md-2" type="primary" @click="getlevel(1)">日视图</el-button>
-          <el-button class="button col-md-2" type="primary" @click="getlevel(2)">周视图</el-button>
-          <el-button class="button col-md-2" type="primary" @click="getlevel(3)">月视图</el-button>
-        </div>
-        <canvas id="planet-chart"></canvas>
+       <drawChart :sensor="sensor"></drawChart>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-import Chart from 'chart.js'
+/*
+父beforeCreate->父created->父beforeMount->子beforeCreate->子created->子beforeMount->子mounted->父mounted */
+/* 子组件更新过程
+父beforeUpdate->子beforeUpdate->子updated->父updated
+父组件更新过程
+父beforeUpdate->父updated
+销毁过程
+父beforeDestroy->子beforeDestroy->子destroyed->父destroyed */
+import drawChart from './chart'
 export default {
   name: 'getInfo',
   data () {
     return {
       data: {},
       infos: [],
-      changeitem: 0,
-      chartdata: {},
-      sensorid: '',
-      id: '',
-      level: 1,
-      time: this.formatDateTime(new Date()),
-      myChart: ''
+      sensor: {}
     }
+  },
+  components: {
+    drawChart
   },
   created () {
     this.init()
   },
   mounted () {
     setTimeout(() => {
-      this.initchart(this.infos[1][0].Id, this.level, this.changeitem)
+      var btn = document.getElementById('tag_a')
+      btn.click()
     }, 1000)
-    // this.intervalid()
+    this.intervalid()
   },
   destroyed () {
     clearInterval(this.intervalid)
@@ -73,14 +74,15 @@ export default {
                   this.infos.push(this.data.Groups[i].Devices)
                   this.infos.push(this.data.Groups[i].Sensors)
                 }
-                let arr=this.infos[1]
-                this.id=arr[0].Id
               }).catch(error => {
                 console.log(error)
               })
           }
         })
         .catch(error => { console.log(error) })
+    },
+    sendItem (item) {
+      this.sensor = item
     },
     deletesign (str) {
       if (typeof (str) === 'string') {
@@ -94,44 +96,6 @@ export default {
       }
       return str
     },
-    initchart (id, level, index) {
-      this.changeitem = index
-      this.chartdata = null
-      this.sensorid = id
-      if (this.myChart) {
-        this.myChart.destroy()
-      }
-      this.$axios.get(this.HOST + '/sensor/' + id + '?level=' + this.level + '&time=' + this.time)
-        .then(res => {
-          if (res.data.status === 0) {
-            this.chartdata = res.data.data
-            var label1 = []
-            var data1 = []
-            for (var i = 0; i < this.chartdata.his.length; i++) {
-              label1.push(this.chartdata.his[i].time)
-              data1.push(this.chartdata.his[i].value)
-            }
-            let data = {
-              labels: label1,
-              datasets: [
-                {
-                  label: 'history Data',
-                  data: data1,
-                  backgroundColor: [
-                    'rgba(71, 183,132,.5)' // Green
-                  ],
-                  borderColor: [
-                    '#47b784'
-                  ],
-                  borderWidth: 3
-                }
-              ]
-            }
-            this.drawChart('planet-chart', data)
-          }
-        })
-        .catch(error => { console.log(error) })
-    },
     close () {
       this.$router.push('/overview')
     },
@@ -139,44 +103,6 @@ export default {
       return setInterval(() => {
         this.init()
       }, 10000)
-    },
-    getlevel (m) {
-      this.level = m
-      this.initchart(this.sensorid, this.level, this.changeitem)
-    },
-    // 时间戳转日期格式
-    formatDateTime (date) {
-      var y = date.getFullYear()
-      var m = date.getMonth() + 1
-      m = m < 10 ? ('0' + m) : m
-      var d = date.getDate()
-      d = d < 10 ? ('0' + d) : d
-      var h = date.getHours()
-      h = h < 10 ? ('0' + h) : h
-      var minute = date.getMinutes()
-      minute = minute < 10 ? ('0' + minute) : minute
-      var second = date.getSeconds()
-      second = second < 10 ? ('0' + second) : second
-      return y +  m +  d +  h + minute
-    },
-    drawChart (chartId, chartData) {
-      var ctx = document.getElementById(chartId)
-      this.myChart = new Chart(ctx, {
-        type: 'line',
-        data: chartData,
-        options: {
-          responsive: true,
-          lineTension: 1,
-          scales: {
-            yAxes: [{
-              ticks: {
-                beginAtZero: true,
-                padding: 25
-              }
-            }]
-          }
-        }
-      })
     }
   }
 }
@@ -221,21 +147,10 @@ h3{
 .astyle{
   text-decoration: underline
 }
-.astyle:active{
-  background-color: aqua;
-  color: rgb(0, 0, 238);
-  text-decoration: none
-}
 .col_value{
   width: 20%;
 }
-/* .table:last-child .col_name{
-  color: rgb(0, 0, 238);
-  text-decoration: underline
-}
-.table:last-child .col_name:hover{
-  color: rgba(0, 0, 238,0.7);
-} */
+
 .chartpart{
  height: 100%;
  flex: 1;
@@ -243,21 +158,4 @@ h3{
  display: flex;
  flex-direction: column;
 }
-.chartpart .toggle{
-height: 80px;
-display: flex;
-}
-.chartpart .toggle .button{
-  margin:auto;
-  text-align: center;
-  transition: all 0.5s;
-}
-.chartpart .toggle .button:focus {
-  background-color: rgb(55, 137, 219);
-  border-color: rgb(55, 137, 219);
-}
-#planet-chart{
-  margin: 30px auto;
-}
-
 </style>
