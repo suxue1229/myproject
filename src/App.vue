596c057a0e92655268416f3f84ae1bf7553b75b4
suxue1229 <suxue7330537@163.com>
2020-05-11 14:17:35 +0800

    修改build后的页面布局(4)  兼容IE10  a标签加 display: inline-block

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,11 @@ export default{
 }
 </script>
 <style>
+*{
+  margin: 0px;
+  padding: 0px;
+  box-sizing: border-box;
+}
 body{
   font-family:"微软雅黑",serif;
   background-color: #f2f6fc;
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class='navbar footer-style'>
+  <nav class='footer-style'>
     <router-link to="/overview" tag="a">{{list[0]}}</router-link>
     <router-link to="/institute" tag="a">{{list[1]}}</router-link>
     <router-link to="/tables" tag="a">{{list[2]}}</router-link>
@@ -53,31 +53,52 @@ export default {
   position: fixed;
   width: 100%;
   height: 50px;
-  display: flex;
-  justify-content: space-evenly;
   border-top: solid 1px rgba(16,46,86,.8);
   padding: 0;
   bottom: 0px;
   background: #304156;
+  display: flex;
 }
 a {
   flex: 1;
+  display: inline-block; /* a标签为行内元素  就是父级设置了flex布局后，子元素就算是行内元素很多浏览器可以把它当做inline-block或者block元素来用，可以直接设置它的宽高，但是还是有些浏览器不支持，所以要设置行内元素的宽度，还是手动设置一下它的display为inline-block或者block. */
+  line-height: 50px;
   text-decoration: none;
   text-align: center;
   font-size: 18px;
   font-weight: bold;
-  line-height: 3em;
   color: #BFCBD9;
 }
+/* 未访问的链接 */
+a:link {
+  text-decoration: none;
+  color: #BFCBD9;
+}
+/* 已访问的链接 */
+a:visited {
+  text-decoration: none;
+  color: #BFCBD9;
+}
+/* 鼠标移动到链接上 */
+a:hover {
+  text-decoration: none;
+  color: #BFCBD9;
+}
+/* 选定的链接 */
+a:active {
+  text-decoration: none;
+  color: #BFCBD9;
+  }
 .footer-style a:nth-child(n+2) {
   border-left: solid 1px  rgba(16,46,86,0.8);
 }
 .iconstyle{
   font-size: 28px;
   text-align: center;
+  line-height: 50px;
   flex: 0.2;
 }
-.active {
-  background-color: #1F2D3D;
+.active{
+   background-color: #1F2D3D;
 }
 </style>
